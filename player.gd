0a08e34a71accd0aa82c78ce851ef8f944505f02
Spring <110597001+Errorsprngtrap@users.noreply.github.com>
2025-06-11 22:44:02 +0200

    p

--- player.gd
+++ player.gd
@@ -1,2 +1,9 @@
 class_name player
 extends CharacterBody2D
+
+@export var speed : float 
+var dir : float
+
+func _physics_process(delta: float) -> void:
+	dir = Input.get_axis("left","right")
+	position.x += speed * dir * delta
